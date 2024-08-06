f46cecb6fbcd8ccf3558a8d332e2e191b712dd8a
Bazurogg <bazurogg@gmail.com>
2024-08-06 16:47:06 +0200

    JAVA -> exo03 -> ✓

--- src/fr/pompey/dev/afpa/exo03/Rectangle.java
+++ src/fr/pompey/dev/afpa/exo03/Rectangle.java
@@ -1,4 +1,56 @@
 package fr.pompey.dev.afpa.exo03;
 
 public class Rectangle {
+
+    public double Longueur;
+    public double Largeur;
+
+    public Rectangle(double Longueur, double Largeur) {
+
+        this.Longueur = Longueur;
+        this.Largeur = Largeur;
+
+    }
+
+    public double getLongueur() {
+        return Longueur;
+    }
+
+    public void setLongueur(double longueur) {
+        Longueur = longueur;
+    }
+
+    public double getLargeur() {
+        return Largeur;
+    }
+
+    public void setLargeur(double largeur) {
+        Largeur = largeur;
+    }
+
+    public double calSurface(){
+
+        return this.Longueur * this.Largeur;
+
+    }
+
+    public double calPerimetre(){
+
+        return (this.Longueur + this.Largeur)*2;
+
+    }
+
+    public void afficher(){
+
+        double surface = calSurface();
+        double perimetre = calPerimetre();
+
+        System.out.println("Caractéristiques du rectangle :");
+        System.out.println("Longueur : " + this.Longueur + " cm");
+        System.out.println("Largeur : " + this.Largeur + " cm");
+        System.out.println("Surface : " + surface + " cm^2");
+        System.out.println("Périmètre : " + perimetre + " cm");
+
+    }
+
 }
---
+++ src/fr/pompey/dev/afpa/exo03/TestRectangle.java
@@ -0,0 +1,15 @@
+package fr.pompey.dev.afpa.exo03;
+
+import fr.pompey.dev.afpa.exo02.Voiture;
+
+public class TestRectangle {
+
+    public static void main(String[] args) {
+
+        Rectangle rectangle1 = new Rectangle(20, 15);
+
+        rectangle1.afficher();
+
+    }
+
+}
