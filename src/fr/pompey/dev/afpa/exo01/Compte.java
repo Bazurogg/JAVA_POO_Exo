476572cc14d431be31c772fe35d4891214503951
Bazurogg <bazurogg@gmail.com>
2024-08-06 14:04:37 +0200

    JAVA -> wip class Compte

--- src/fr/pompey/dev/afpa/exo01/Compte.java
+++ src/fr/pompey/dev/afpa/exo01/Compte.java
@@ -1,4 +1,26 @@
 package fr.pompey.dev.afpa.exo01;
 
+/**
+ * Classe Compte
+ * @author GenceFabien - CDA23016
+ * classe qui représente un compte bancaire
+ */
+
 public class Compte {
+
+    /**
+     * Déclaration des éléments static
+     */
+
+    // attributs
+    public double solde ;
+
+    public static void main(String[] args) {
+
+        Compte compte = new Compte();
+
+        System.out.println(compte);
+
+    }
+
 }
