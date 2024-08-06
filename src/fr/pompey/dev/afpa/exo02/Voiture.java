27137acb70e05bff7756a9d6480670027f297fcf
Bazurogg <bazurogg@gmail.com>
2024-08-06 15:27:57 +0200

    AVA -> exo02 -> getter&setter

--- src/fr/pompey/dev/afpa/exo02/Voiture.java
+++ src/fr/pompey/dev/afpa/exo02/Voiture.java
@@ -1,4 +1,32 @@
 package fr.pompey.dev.afpa.exo02;
 
 public class Voiture {
+
+
+    public char marque;
+    public double prix;
+
+    public Voiture() {
+
+    };
+
+    // Getter & Setter
+    public char getMarque() {
+        return marque;
+    }
+
+    public void setMarque(char marque) {
+        this.marque = marque;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+
+
+
 }
