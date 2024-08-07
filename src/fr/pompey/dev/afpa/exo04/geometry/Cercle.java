22054ebddae89790a1789017d1a18223b5407733
Bazurogg <bazurogg@gmail.com>
2024-08-07 08:34:46 +0200

    JAVA -> exo04 -> editing class "Cercle"

--- src/fr/pompey/dev/afpa/exo04/geometry/Cercle.java
+++ src/fr/pompey/dev/afpa/exo04/geometry/Cercle.java
@@ -2,4 +2,15 @@ package fr.pompey.dev.afpa.exo04.geometry;
 
 public class Cercle {
 
+    private Point centre;
+    private double rayon;
+
+    public Cercle(Point centre, double rayon) {
+
+        this.centre = centre;
+        this.rayon = rayon;
+        
+    }
+
+
 }
