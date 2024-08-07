ad4e42f7913f09c0349c263c1fc18085e6b4dce4
Bazurogg <bazurogg@gmail.com>
2024-08-07 09:07:51 +0200

    JAVA -> exo04 -> building Class "Point"

--- src/fr/pompey/dev/afpa/exo04/geometry/Point.java
+++ src/fr/pompey/dev/afpa/exo04/geometry/Point.java
@@ -5,4 +5,36 @@ public class Point {
     private double Abscisse;
     private double Ordonnee;
 
+    public Point(double Abscisse, double Ordonnee) {
+
+        this.Abscisse = Abscisse;
+        this.Ordonnee = Ordonnee;
+
+    }
+
+    public double getAbscisse() {
+
+        return Abscisse;
+
+    }
+
+    public void setAbscisse(double abscisse) {
+
+        Abscisse = abscisse;
+
+    }
+
+    public double getOrdonnee() {
+
+        return Ordonnee;
+
+    }
+
+    public void setOrdonnee(double ordonnee) {
+
+        Ordonnee = ordonnee;
+
+    }
+
+
 }
